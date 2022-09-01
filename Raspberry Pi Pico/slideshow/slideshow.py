ae3328944e77eb03fc46091c8b60fa5a445febd6
educ8s <info@educ8s.tv>
2022-09-01 12:11:23 +0300

    v1.02 Video code

---
+++ Raspberry Pi Pico/slideshow/slideshow.py
@@ -0,0 +1,46 @@
+# This script supports the Raspberry Pi Pico board and the Lilygo ESP32-S2 board
+# Raspberry Pi Pico:http://educ8s.tv/part/RaspberryPiPico
+# ESP32-S2 Board: http://educ8s.tv/part/esp32s2
+
+import board,busio, os
+from time import sleep
+import adafruit_ili9341
+import displayio
+import time
+
+board_type = os.uname().machine
+print(f"Board: {board_type}")
+
+if 'Pico' in board_type:
+    mosi_pin, clk_pin, reset_pin, cs_pin, dc_pin = board.GP11, board.GP10, board.GP17, board.GP18, board.GP16
+elif 'ESP32-S2' in board_type:
+    mosi_pin, clk_pin, reset_pin, cs_pin, dc_pin = board.IO35, board.IO36, board.IO38, board.IO34, board.IO37    
+else:
+    mosi_pin, clk_pin, reset_pin, cs_pin, dc_pin = board.GP11, board.GP10, board.GP17, board.GP18, board.GP16
+    print("This board is not supported. Change the pin definitions above.")
+
+displayio.release_displays()
+
+spi = busio.SPI(clock=clk_pin, MOSI=mosi_pin)
+
+display_bus = displayio.FourWire(spi, command=dc_pin, chip_select=cs_pin, reset=reset_pin)
+
+display = adafruit_ili9341.ILI9341(display_bus, width=240, height=320, rotation=270)
+
+bitmap = displayio.OnDiskBitmap("/0.bmp")
+bitmap1 = displayio.OnDiskBitmap("/1.bmp")
+bitmap2 = displayio.OnDiskBitmap("/2.bmp")
+group = displayio.Group()
+display.show(group)
+
+while True:
+    tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
+    group.append(tile_grid)
+    sleep(8)
+    tile_grid = displayio.TileGrid(bitmap1, pixel_shader=bitmap1.pixel_shader)
+    group.append(tile_grid)
+    sleep(8)
+    tile_grid = displayio.TileGrid(bitmap2, pixel_shader=bitmap2.pixel_shader)
+    group.pop()
+    group.append(tile_grid)
+    sleep(8)
