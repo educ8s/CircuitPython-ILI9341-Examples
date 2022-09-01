8ea54522e1c91824a974a9dc1f847903a85329b8
educ8s <info@educ8s.tv>
2022-09-01 12:37:05 +0300

    v1.04
    Code cleanup

--- simpletest/simpletest.py
+++ simpletest/simpletest.py
@@ -1,17 +1,24 @@
-#Raspberry PI Pico version
+# This script supports the Raspberry Pi Pico board and the Lilygo ESP32-S2 board
+# Raspberry Pi Pico: http://educ8s.tv/part/RaspberryPiPico
+# ESP32-S2 Board: http://educ8s.tv/part/esp32s2
 
-import board, busio, displayio
+import board, busio, displayio, os
 import terminalio #Just a font
 import adafruit_ili9341
 from adafruit_display_text import label
 
 displayio.release_displays()
 
-mosi_pin = board.GP11
-clk_pin = board.GP10
-reset_pin = board.GP17
-cs_pin = board.GP18
-dc_pin = board.GP16
+board_type = os.uname().machine
+print(f"Board: {board_type}")
+
+if 'Pico' in board_type:
+    mosi_pin, clk_pin, reset_pin, cs_pin, dc_pin = board.GP11, board.GP10, board.GP17, board.GP18, board.GP16
+elif 'ESP32-S2' in board_type:
+    mosi_pin, clk_pin, reset_pin, cs_pin, dc_pin = board.IO35, board.IO36, board.IO38, board.IO34, board.IO37    
+else:
+    mosi_pin, clk_pin, reset_pin, cs_pin, dc_pin = board.GP11, board.GP10, board.GP17, board.GP18, board.GP16
+    print("This board is not supported. Change the pin definitions above.")
 
 spi = busio.SPI(clock=clk_pin, MOSI=mosi_pin)
 
